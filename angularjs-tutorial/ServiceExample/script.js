6c308d80b5c346ebe94e3530fb86699e87d484d3
Yu Lu <yulu8798@gmail.com>
2015-07-19 13:32:13 +0800

    add service example

---
+++ angularjs-tutorial/ServiceExample/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yulu on 19/7/15.
+ */
+angular.module('serviceExample', [])
+    .controller('MyController', ['$scope', 'notify', function($scope, notify) {
+        $scope.callNotify = function(msg) {
+            notify(msg);
+        };
+    }])
+    .factory('notify', ['$window', function(win) {
+        var msgs = [];
+
+        return function(msg) {
+            msgs.push(msg);
+            if (msgs.length == 3) {
+                win.alert(msgs.join("\n"));
+                msgs = [];
+            }
+        };
+    }]);
